5a0c81daa7b3b2aed4c51b4cfc76f972997271b5
Deon <deonsummers91@gmail.com>
2019-05-15 06:11:17 -0500

    fix some test

--- contracts/Multisend.sol
+++ contracts/Multisend.sol
@@ -10,21 +10,31 @@ contract Multisend {
   uint private _fee;
   mapping(address => mapping(address => uint256)) public balances;
 
+
+  /**
+  * @param initialFee amount of fees taken per transaction in basis points
+  */
   constructor(uint256 initialFee) public {
     _owner = msg.sender;
     _fee = initialFee;
   }
 
-  function deposit(address[] calldata tokenDepositAddress, uint256[] calldata tokenDepositAmount) external payable {
+  function deposit(address[] memory tokenDepositAddress, uint256[] memory tokenDepositAmount) public payable {
     require(tokenDepositAddress.length == tokenDepositAmount.length);
-    balances[msg.sender][address(0)] = balances[msg.sender][address(0)].add(msg.value);
+    if(msg.value != 0) {
+      uint256 etherFee = msg.value.div(10000).mul(_fee);
+      balances[msg.sender][address(0)] = balances[msg.sender][address(0)].add(msg.value.sub(etherFee));
+      balances[address(this)][address(0)] = balances[address(this)][address(0)].add(etherFee);
+    }
     for (uint i=0;i<tokenDepositAddress.length;i++) {
+      uint256 tokenFee = tokenDepositAmount[i].div(10000).mul(_fee);
       IERC20(tokenDepositAddress[i]).transferFrom(msg.sender, address(this), tokenDepositAmount[i]);
-      balances[msg.sender][tokenDepositAddress[i]] = balances[msg.sender][tokenDepositAddress[i]].add(tokenDepositAmount[i]);
+      balances[msg.sender][tokenDepositAddress[i]] = balances[msg.sender][tokenDepositAddress[i]].add(tokenDepositAmount[i].sub(tokenFee));
+      balances[address(this)][tokenDepositAddress[i]] = balances[address(this)][tokenDepositAddress[i]].add(tokenFee);
     }
   }
 
-  function sendPayment(address[] calldata tokens, address payable[] calldata recipients, uint256[] calldata amounts) external payable returns (bool) {
+  function sendPayment(address[] memory tokens, address payable[] memory recipients, uint256[] memory amounts) public payable returns (bool) {
     require(tokens.length == recipients.length);
     require(tokens.length == amounts.length);
     uint256 total_ether_amount = 0;
@@ -39,11 +49,11 @@ contract Multisend {
         balances[msg.sender][address(0)] = balances[msg.sender][address(0)].sub(amounts[i]);
       }
     }
-    require(total_ether_amount == msg.value);
   }
 
   function depositAndSendPayment(address[] calldata tokenDepositAddress, uint256[] calldata tokenDepositAmount, address[] calldata tokens, address payable[] calldata recipients, uint256[] calldata amounts) external payable returns (bool) {
-      this.deposit(tokenDepositAddress, tokenDepositAmount);
+      deposit(tokenDepositAddress, tokenDepositAmount);
+      sendPayment(tokens, recipients, amounts);
   }
 
   function withdrawTokens(address payable[] calldata tokenAddresses) external {
@@ -54,6 +64,12 @@ contract Multisend {
     }
   }
 
+  /*** CONSTANT METHODS **/
+  function getBalance(address _owner, address _address) external view returns (uint256) {
+    return balances[_owner][_address];
+  }
+
+  /*** OWNER METHODS **/
   function withdrawEther() external onlyOwner {
     _owner.transfer(address(this).balance);
   }
--- test/multisend.js
+++ test/multisend.js
@@ -2,8 +2,7 @@ const Multisend = artifacts.require("Multisend");
 const TestToken = artifacts.require("TestToken");
 
 
-const DECIMALS = 10^18
-const fees = 0.05* DECIMALS;
+const fees = 5;
 
 contract('Multisend', (accounts) => {
   const ZERO_ADDRESS = "0x0000000000000000000000000000000000000000"
@@ -16,16 +15,16 @@ contract('Multisend', (accounts) => {
   let employee4 = accounts[4];
 
   beforeEach(async() => {
-    multisend = await Multisend.new(web3.utils.toBN(5));
-    token1 = await TestToken.new(web3.utils.toBN(1000* DECIMALS))
-    token2 = await TestToken.new(web3.utils.toBN(1000* DECIMALS))
-    token3 = await TestToken.new(web3.utils.toBN(1000* DECIMALS))
-    token4 = await TestToken.new(web3.utils.toBN(1000* DECIMALS))
+    multisend = await Multisend.new(fees);
+    token1 = await TestToken.new(web3.utils.toWei("1000"))
+    token2 = await TestToken.new(web3.utils.toWei("1000"))
+    token3 = await TestToken.new(web3.utils.toWei("1000"))
+    token4 = await TestToken.new(web3.utils.toWei("1000"))
 
-    await token1.approve(multisend.address, web3.utils.toBN(1000* DECIMALS))
-    await token2.approve(multisend.address, web3.utils.toBN(1000* DECIMALS))
-    await token3.approve(multisend.address, web3.utils.toBN(1000* DECIMALS))
-    await token4.approve(multisend.address, web3.utils.toBN(1000* DECIMALS))
+    await token1.approve(multisend.address, web3.utils.toWei("1000"))
+    await token2.approve(multisend.address, web3.utils.toWei("1000"))
+    await token3.approve(multisend.address, web3.utils.toWei("1000"))
+    await token4.approve(multisend.address, web3.utils.toWei("1000"))
   });
 
   it("should have an owner", async() => {
@@ -56,37 +55,45 @@ contract('Multisend', (accounts) => {
   it("should transfer one token to one person", async() =>  {
     let beginning_balance = await token1.balanceOf(employer);
     let allowance = await token1.allowance(employer, multisend.address);
-    assert.deepEqual(allowance, web3.utils.toBN(1000*DECIMALS));
-    assert.equal(+beginning_balance, web3.utils.toBN(1000* DECIMALS));
+    assert.deepEqual(allowance.toString(), web3.utils.toWei("1000"));
+    assert.deepEqual(beginning_balance.toString(), web3.utils.toWei("1000"));
 
     await multisend.depositAndSendPayment(
           [token1.address],
-          [5*DECIMALS],
+          [web3.utils.toWei("52")],
           [token1.address],
           [employee1],
-          [web3.utils.toBN(5* DECIMALS)], {from: employer});
+          [web3.utils.toWei("50")], {from: employer});
 
     let employer_balance = await token1.balanceOf(employer);
     let employee_balance = await token1.balanceOf(employee1);
 
-    assert.equal(+employer_balance, web3.utils.toBN(web3.utils.toBN(web3.utils.toBN(950* DECIMALS))));
-    assert.equal(+employee_balance, web3.utils.toBN(50* DECIMALS));
+    let contract_fee_balance = await multisend.getBalance(multisend.address, token1.address);
+    assert.equal(+contract_fee_balance, +web3.utils.toWei((52/10000*fees).toString()));
+    assert.deepEqual(+employer_balance, +web3.utils.toWei("948"));
+    assert.deepEqual(+employee_balance, +web3.utils.toWei("50"));
   });
 
-  xit("should transfer ether to one person", async() =>  {
+  it("should transfer ether to one person", async() =>  {
     let beginning_employee_balance = await web3.eth.getBalance(employee1);
 
-    await multisend.depositAndSendPayment([ZERO_ADDRESS],[employee1], [50* DECIMALS], {from: employer, value: web3.utils.toBN(50* DECIMALS)});
+    await multisend.depositAndSendPayment(
+      [],
+      [],
+      [ZERO_ADDRESS],
+      [employee1],
+      [web3.utils.toWei(".5")], {from: employer, value: web3.utils.toWei("1")});
 
     let employee_balance = await web3.eth.getBalance(employee1);
 
     assert.notDeepEqual(beginning_employee_balance, employee_balance);
-    assert.equal(employee_balance, +beginning_employee_balance + web3.utils.toBN(50* DECIMALS));
+    assert.equal(employee_balance, +beginning_employee_balance + +web3.utils.toWei(".5"));
   });
 
-  xit("should not be able to transfer more ether than has been sent", async() => {
+  it("should not be able to transfer more ether than has been sent", async() => {
+    await multisend.deposit([], [], {value: 1000, from: employee1});
     try {
-      await multisend.depositAndSendPayment([ZERO_ADDRESS],[employee1], [50* DECIMALS], {from: employer, value: 20});
+      await multisend.depositAndSendPayment([],[],[ZERO_ADDRESS],[employee1], ["50"], {from: employer, value: 20});
       throw null;
     } catch (e) {
       if (e == null) assert.equal(0, 1, "Transaction did not error");
@@ -94,55 +101,61 @@ contract('Multisend', (accounts) => {
     }
   });
 
-  xit("should transfer one token and ether to one person", async() =>  {
+  it("should transfer one token and ether to one person", async() =>  {
     let beginning_balance = await token1.balanceOf(employer);
     let beginning_ether_balance = await web3.eth.getBalance(employee1);
 
-    assert.equal(+beginning_balance, web3.utils.toBN(1000* DECIMALS));
+    assert.equal(+beginning_balance, web3.utils.toWei("1000"));
 
-    await multisend.depositAndSendPayment([token1.address, ZERO_ADDRESS],[employee1, employee1], [50* DECIMALS, web3.utils.toBN(50* DECIMALS)], {from: employer, value: web3.utils.toBN(50* DECIMALS)});
+    await multisend.depositAndSendPayment(
+        [token1.address],
+        ["50"],
+        [token1.address, ZERO_ADDRESS],
+        [employee1, employee1],
+        ["50", web3.utils.toWei("1")],
+        {from: employer, value: web3.utils.toWei("1.1")});
 
     let employer_balance = await token1.balanceOf(employer);
     let employee_balance = await token1.balanceOf(employee1);
     let employee_ether_balance = await web3.eth.getBalance(employee1);
 
-    assert.equal(+employer_balance, web3.utils.toBN(web3.utils.toBN(950* DECIMALS)));
-    assert.equal(+employee_balance, web3.utils.toBN(50* DECIMALS));
-    assert.equal(employee_ether_balance, +beginning_ether_balance + web3.utils.toBN(50* DECIMALS));
+    assert.equal(+employer_balance, web3.utils.toWei(web3.utils.toWei("950")));
+    assert.equal(+employee_balance, web3.utils.toWei("50"));
+    assert.equal(employee_ether_balance, +beginning_ether_balance + web3.utils.toWei("50"));
   });
 
   xit("should transfer two tokens to one person", async() =>  {
     let beginning_token_balance = await token1.balanceOf(employer);
     let beginning_token2_balance = await web3.eth.getBalance(employee1);
 
-    await multisend.depositAndSendPayment([token1.address, token2.address],[employee1, employee1], [50* DECIMALS, web3.utils.toBN(50* DECIMALS)], {from: employer});
+    await multisend.depositAndSendPayment([token1.address, token2.address],[employee1, employee1], ["50", web3.utils.toWei("50")], {from: employer});
 
     let employer_token1_balance = await token1.balanceOf(employer);
     let employee_token1_balance = await token1.balanceOf(employee1);
     let employer_token2_balance = await token2.balanceOf(employer);
     let employee_token2_balance = await token2.balanceOf(employee1);
 
-    assert.equal(+employer_token1_balance, web3.utils.toBN(web3.utils.toBN(950* DECIMALS)));
-    assert.equal(+employer_token2_balance, web3.utils.toBN(web3.utils.toBN(950* DECIMALS)));
-    assert.equal(+employee_token1_balance, web3.utils.toBN(50* DECIMALS));
-    assert.equal(+employee_token2_balance, web3.utils.toBN(50* DECIMALS));
+    assert.equal(+employer_token1_balance, web3.utils.toWei(web3.utils.toWei("950")));
+    assert.equal(+employer_token2_balance, web3.utils.toWei(web3.utils.toWei("950")));
+    assert.equal(+employee_token1_balance, web3.utils.toWei("50"));
+    assert.equal(+employee_token2_balance, web3.utils.toWei("50"));
   });
 
   xit("should transfer one tokens to multiple people", async() =>  {
     let beginning_token_balance = await token1.balanceOf(employer);
     let beginning_token2_balance = await web3.eth.getBalance(employee1);
 
-    await multisend.depositAndSendPayment([token1.address, token1.address, token1.address],[employee1, employee2, employee3], [50* DECIMALS, web3.utils.toBN(50* DECIMALS), web3.utils.toBN(50* DECIMALS)], {from: employer});
+    await multisend.depositAndSendPayment([token1.address, token1.address, token1.address],[employee1, employee2, employee3], ["50", web3.utils.toWei("50"), web3.utils.toWei("50")], {from: employer});
 
     let employer_token1_balance = await token1.balanceOf(employer);
     let employee1_token1_balance = await token1.balanceOf(employee1);
     let employee2_token1_balance = await token1.balanceOf(employee2);
     let employee3_token1_balance = await token1.balanceOf(employee3);
 
-    assert.equal(+employer_token1_balance, web3.utils.toBN(850* DECIMALS));
-    assert.equal(+employee1_token1_balance, web3.utils.toBN(50* DECIMALS));
-    assert.equal(+employee2_token1_balance, web3.utils.toBN(50* DECIMALS));
-    assert.equal(+employee3_token1_balance, web3.utils.toBN(50* DECIMALS));
+    assert.equal(+employer_token1_balance, web3.utils.toWei("850"));
+    assert.equal(+employee1_token1_balance, web3.utils.toWei("50"));
+    assert.equal(+employee2_token1_balance, web3.utils.toWei("50"));
+    assert.equal(+employee3_token1_balance, web3.utils.toWei("50"));
   });
 
   xit("should revert if sending arrays of different lengths", async() => {
@@ -156,7 +169,7 @@ contract('Multisend', (accounts) => {
     await multisend.depositAndSendPayment(
       [token1.address, token2.address, token1.address],
       [employee1, employee2, employee3],
-      [50* DECIMALS, web3.utils.toBN(50* DECIMALS), web3.utils.toBN(50* DECIMALS)],
+      ["50", web3.utils.toWei("50"), web3.utils.toWei("50")],
       {from: employer});
 
     let employer_token1_balance = await token1.balanceOf(employer);
@@ -166,27 +179,27 @@ contract('Multisend', (accounts) => {
     let employee3_token1_balance = await token1.balanceOf(employee3);
 
     assert.equal(+employer_token1_balance, 900);
-    assert.equal(+employer_token2_balance, web3.utils.toBN(web3.utils.toBN(950* DECIMALS)));
-    assert.equal(+employee1_token1_balance, web3.utils.toBN(50* DECIMALS));
-    assert.equal(+employee2_token2_balance, web3.utils.toBN(50* DECIMALS));
-    assert.equal(+employee3_token1_balance, web3.utils.toBN(50* DECIMALS));
+    assert.equal(+employer_token2_balance, web3.utils.toWei(web3.utils.toWei("950")));
+    assert.equal(+employee1_token1_balance, web3.utils.toWei("50"));
+    assert.equal(+employee2_token2_balance, web3.utils.toWei("50"));
+    assert.equal(+employee3_token1_balance, web3.utils.toWei("50"));
   });
 
   xit("should transfer two tokens and ether to multiple people", async() =>  {
     let beginning_token_balance = await token1.balanceOf(employer);
     let beginning_token2_balance = await web3.eth.getBalance(employee1);
 
-    await multisend.depositAndSendPayment([token1.address, token1.address, token1.address],[employee1, employee2, employee3], [50* DECIMALS, web3.utils.toBN(50* DECIMALS), web3.utils.toBN(50* DECIMALS)], {from: employer});
+    await multisend.depositAndSendPayment([token1.address, token1.address, token1.address],[employee1, employee2, employee3], ["50", web3.utils.toWei("50"), web3.utils.toWei("50")], {from: employer});
 
     let employer_token1_balance = await token1.balanceOf(employer);
     let employee1_token1_balance = await token1.balanceOf(employee1);
     let employee2_token1_balance = await token1.balanceOf(employee2);
     let employee3_token1_balance = await token1.balanceOf(employee3);
 
-    assert.equal(+employer_token1_balance, web3.utils.toBN(850* DECIMALS));
-    assert.equal(+employee1_token1_balance, web3.utils.toBN(50* DECIMALS));
-    assert.equal(+employee2_token1_balance, web3.utils.toBN(50* DECIMALS));
-    assert.equal(+employee3_token1_balance, web3.utils.toBN(50* DECIMALS));
+    assert.equal(+employer_token1_balance, web3.utils.toWei("850"));
+    assert.equal(+employee1_token1_balance, web3.utils.toWei("50"));
+    assert.equal(+employee2_token1_balance, web3.utils.toWei("50"));
+    assert.equal(+employee3_token1_balance, web3.utils.toWei("50"));
   });
 
   xit("should be able to withdraw tokens", async() => {
@@ -198,7 +211,7 @@ contract('Multisend', (accounts) => {
   xit("should be able to sending multiple tokens", async() => {
     await multisend.depositAndSendPayment(new Array(30).fill(token1.address),
                                 new Array(30).fill(employee1),
-                                new Array(30).fill(2* DECIMALS),
+                                new Array(30).fill(web3.utils.toWei("2")),
                                 {from: employer});
   });
 
